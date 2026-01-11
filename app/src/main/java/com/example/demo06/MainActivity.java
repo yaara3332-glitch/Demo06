534cb684acc95b986c7959e8a0008b7ced54baa9
yaara <yaara3332@gmail.com>
2026-01-11 18:33:02 +0200

    Initial commit1

---
+++ app/src/main/java/com/example/demo06/MainActivity.java
@@ -0,0 +1,37 @@
+package com.example.demo06;
+
+import android.os.Bundle;
+import android.widget.TextView;
+import android.view.View;
+import android.widget.Button;
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class MainActivity extends AppCompatActivity {
+
+    TextView textView;
+    Button button;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+
+        textView = findViewById(R.id.textView);
+        button = findViewById(R.id.button);
+    }
+
+    public void clicked(View view){
+        textView.setText("oh yeah iv been clicked");
+    }
+}
